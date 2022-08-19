ef3d9333f649927130f41da95c5b667a30ae5c3d
pcw11211@yahoo.com <root@localhost>
2022-08-19 19:14:24 -0400

    config multyline

--- status.sh
+++ status.sh
@@ -9,18 +9,44 @@ printSection()
     [[ $found == false && "$line" != "[$section]" ]] &&  continue
     [[ $found == true && "${line:0:1}" = '[' ]] && break
     found=true
-    echo "$line" | cut -d ']' -f 2 | tr -dc '[[:print:]]'
+    [[  $found == true &&  "$line" == "[$section]" ]] &&  continue
+    echo $line
+    #echo 123
+    #string="$(echo $line)"
+    #string=$(echo "$string" | sed 1d)
+    #echo $string
   done
 }
+
+printSection1()
+{
+  section="$1"
+  found=false
+  while read line
+  do
+    [[ $found == false && "$line" != "[$section]" ]] &&  continue
+    [[ $found == true && "${line:0:1}" = '[' ]] && break
+    found=true
+    echo "$line"
+  done
+}
+
+
+
+
 
 my_path="$(dirname "$(realpath "$0")")"
 #echo $my_path
 
 for FILE in $my_path/sensors/*; do
   #echo $FILE;
-  sensor_name=$(printSection name <$FILE)
-  sensor_desc=$(printSection description <$FILE)
+#  printSection name <$FILE
+  sensor_name="$(printSection name <$FILE)"
+#  echo "$sensor_name"
+
+  sensor_desc="$(printSection description <$FILE)"
   echo "$sensor_name ($sensor_desc)"
+
   sensor_exec=$(printSection "exec" <$FILE)
 #  echo $my_path/bin/$sensor_exec
 
@@ -30,5 +56,9 @@ for FILE in $my_path/sensors/*; do
     sensor_value=$($my_path/bin/$sensor_exec)
   fi
   echo $sensor_value
+
+  #printSection alarm  <$FILE
+  sensor_alarm=$(printSection alarm <$FILE)
+  echo "$sensor_alarm"
   echo
 done
